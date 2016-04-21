4a3a56e51cf456db25d49b75510ee3734880f69c
Stephen Pirozzi <spirozzi@umass.edu>
2016-04-20 23:09:00 -0400

    basic page serving and front end functionality

---
+++ src/public/assets/clientjs/index.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	// Global variables
+
+	// chosen elev trend, changed by elev radio buttons, default to "FLAT"
+	var elev_trend = 'FLAT';
+	// Set up WebSocket connection with server
+	var socket = io.connect('http://localhost:3000');
+
+	// Register WebSocket event handlers
+
+	// receives the requested elev-optimized routes 
+	socket.on('receive-routes-success', function (data) {
+		// parse routes
+		// draw top ranked route on map, display details
+		// list other routes on sidebar
+	}
+
+	// true or false sent from server if route request status succeeds or fails
+  socket.on('receive-routes-failure', function (data) {
+		if (data.status === false) {
+			alert('Failed to find routes, please try again.');
+		}
+  });
+
+  // Register radio button form handler
+  $('#postnowform').submit(function(event) {
+  	var trend = $("input[type='radio'][name='trendoption']:checked").val();
+  	
+		//socket.emit('request-routes', { elev: elev_trend });
+  	event.preventDefault();
+  });
+});
