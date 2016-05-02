95035b7834f351256b7d4db0be5f6220c968509f
Stephen Pirozzi <stephenpirozzi@gmail.com>
2016-05-01 20:46:47 -0400

    make indentation consistent across files

--- src/public/assets/clientjs/index.js
+++ src/public/assets/clientjs/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	// Global variables
+	// Global Variables
 
 	// chosen elev trend, changed by elev radio buttons, default to "FLAT"
 	var elev_trend = 'FLAT';
@@ -8,25 +8,26 @@ $(document).ready(function() {
 
 	// Register WebSocket event handlers
 
-	// receives the requested elev-optimized routes 
+	// receives the requested elevation-optimized routes
 	socket.on('receive-routes-success', function (data) {
 		// parse routes
 		// draw top ranked route on map, display details
 		// list other routes on sidebar
 	}
 
-	// true or false sent from server if route request status succeeds or fails
-  socket.on('receive-routes-failure', function (data) {
+	// true or false sent from server if route request succeeds or fails
+	socket.on('receive-routes-failure', function (data) {
 		if (data.status === false) {
 			alert('Failed to find routes, please try again.');
 		}
-  });
+	});
 
-  // Register radio button form handler
-  $('#postnowform').submit(function(event) {
-  	var trend = $("input[type='radio'][name='trendoption']:checked").val();
-  	
-		//socket.emit('request-routes', { elev: elev_trend });
-  	event.preventDefault();
-  });
+	// jQuery Event Handlers
+
+	// register radio button form handler
+	$('#postnowform').submit(function(event) {
+		var trend = $("input[type='radio'][name='trendoption']:checked").val();
+		// socket.emit('request-routes', { elev: elev_trend });
+		event.preventDefault();
+	});
 });
